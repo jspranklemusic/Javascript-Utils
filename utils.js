ef85eb4c5b9a8a32be44760f0d6f3061ff5a36e4
Josiah Sprankle <75324120+jspranklemusic@users.noreply.github.com>
2021-07-28 16:58:15 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+//making a random array
 function randomizeArray(oldArr=[1,2,3,4,5,6,7,8,9],newArr=[]){
     if(!oldArr.length){
         return newArr;
@@ -7,3 +8,13 @@ function randomizeArray(oldArr=[1,2,3,4,5,6,7,8,9],newArr=[]){
         return randomizeArray(oldArr, newArr);
     }
 }
+
+//converting unsafe html to safe html
+function escapeHtml(unsafe="<script>console.log('hacked!')</script>") {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
